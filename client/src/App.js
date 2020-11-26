eb1aa30eb0689fecaf6c535e8363a4d7adb6f21c
harrykim14 <harrykim14@korea.ac.kr>
2020-11-26 14:32:05 +0900

    20. 11. 26
    client update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
+    <>
+    <Header/>
     <div>Wemo React Project, Setup.</div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            WeMo React Project @2020.12
+        </div>
+    )
+}
+
+export default Footer
