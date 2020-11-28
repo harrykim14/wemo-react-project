88f232aafd5fa55529273ebc25dd6fc0571f78be
harrykim14 <harrykim14@korea.ac.kr>
2020-11-28 11:00:30 +0900

    20. 11. 28
    Update login page with material ui

--- client/src/component/Views/LoginPage/Section/LoginMain.js
+++ client/src/component/Views/LoginPage/Section/LoginMain.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import WeMoLogo from './pics/WemoLogo.png';
+import React, { useState, useEffect } from 'react'
+import { TextField , Button } from "@material-ui/core";
+import WeMoLogo200 from './pics/WemoLogo_200px.png'
+import WeMoLogo400 from './pics/WemoLogo_400px.png'
 
 function LoginMain() {
+
+    const [logo, setLogo] = useState(WeMoLogo400);
+    useEffect(() => {
+        (window.innerWidth > 480) ? setLogo(WeMoLogo400) : setLogo(WeMoLogo200);
+    }, [])
+
     return (
-        <div style= {{ border : '2px solid black', height: '65vh', width: '91.5%', margin: '0vh 5vh 0vh 5vh', borderTopColor:'white', borderRadius:'0px 0px 5px 5px'}}>
-        <img src ={WeMoLogo} alt = "WeMoLogo" style= {{ display:'block', margin: '0 auto', paddingTop: '50px'}}/>            
-        <div style ={{textAlign:'center', marginTop:'50px'}}>
-        <form>
-            <label>아이디&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-            <input type='text'/><br/>
-            <label>비밀번호&nbsp;</label>
-            <input type='password'/><br/>
-        </form>
-        </div>
+        <div style= {{ border : '2px solid black', height: '70vh', margin: '0vh 5vh 0vh 5vh', borderTopColor:'white', borderRadius:'0px 0px 5px 5px'}}>
+            <img src ={logo} alt = "WeMoLogo" style= {{ display:'block', margin: '0 auto', paddingTop: '50px'}}/>            
+            <div style ={{textAlign:'center', marginTop:'50px'}}>
+                <form>
+                    <TextField label="아이디" required /><br/>
+                    <TextField label="비밀번호" required /><br/><br/>
+                    <Button variant="outlined">로그인</Button>&nbsp;
+                    <Button variant="outlined">회원가입</Button>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/component/Views/LoginPage/Section/LoginTop.js
+++ client/src/component/Views/LoginPage/Section/LoginTop.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import close from './pics/close.png';
-import crop from './pics/crop.png';
-import minimize from './pics/minimize.png';
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import CropSquareIcon from '@material-ui/icons/CropSquare';
+import CloseIcon from '@material-ui/icons/Close';
+import MinimizeIcon from '@material-ui/icons/Minimize';
 
 function LoginTop() {
     const thisYear = new Date().getFullYear() - 2000;
     const thisMonth = new Date().getMonth()+1;
     const thisDate = new Date().getDate();
     const today = `${thisYear}.${thisMonth < 10 ? 0+thisMonth : thisMonth}.${thisDate<10 ? 0+thisDate : thisDate}.`
-
+    const iconStyle = { float: 'right'};
     return (
         <div style = {{ 
             border: '2px solid black',
             margin : '10vh 5vh 0vh 5vh',
-            width : '90%',
             borderRadius: '10px 10px 0px 0px',
-            height: '20px',
-            lineHeight : '20px',
+            height: '25px',
+            lineHeight : '25px',
             padding : '5px',
-            fontWeight: '700',
             backgroundColor:'pink'
             }}>
-                <span>{today}</span>
-                <img src = {close} alt="close" style = {{float:'right'}}/>
-                <img src = {crop} alt="crop" style = {{float:'right'}}/>
-                <img src = {minimize} alt='minimize' style = {{float:'right'}}/>
+                <Typography variant ={'body1'} 
+                            style = {{fontWeight: '700', fontSize: '1.2rem'}}>
+                    {today}
+                    <CloseIcon style = {iconStyle}/>
+                    <CropSquareIcon style = {iconStyle}/>
+                    <MinimizeIcon style = {iconStyle}/>
+                </Typography>
         </div>
     )
 }
