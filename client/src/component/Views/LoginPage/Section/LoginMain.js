29ccb7f66496e936265ff335c62d9fa5fd9bde87
harrykim14 <harrykim14@korea.ac.kr>
2020-12-08 17:58:48 +0900

    20. 12. 08
    minor update (bad condition day)

--- client/src/component/Views/LoginPage/Section/LoginMain.js
+++ client/src/component/Views/LoginPage/Section/LoginMain.js
@@ -6,9 +6,25 @@ import WeMoLogo400 from '../../../../Images/WemoLogo_400px.png'
 function LoginMain(props) {
 
     const [logo, setLogo] = useState(WeMoLogo400);
+    const [UserId, setUserId] = useState('');
+    const [UserPassword, setUserPassword] = useState('');
+
     useEffect(() => {
         (window.innerWidth > 480) ? setLogo(WeMoLogo400) : setLogo(WeMoLogo200);
     }, [])
+    
+    const idInputHandler = (event) => {
+        setUserId(event.currentTarget.value);
+    }
+
+    const pwInputHandler = (event) => {
+        setUserPassword(event.currentTarget.value);
+    }
+
+    const loginHandler = () => {
+        if (!UserId && !UserPassword) alert('아이디와 비밀번호를 모두 입력해주세요.')
+        console.log(UserId, UserPassword);
+    }
 
     
 
@@ -16,10 +32,10 @@ function LoginMain(props) {
         <div style= {{ border : '2px solid black', height: '70vh', margin: '0vh 5vh 0vh 5vh', borderTopColor:'white', borderRadius:'0px 0px 5px 5px'}}>
             <img src ={logo} alt = "WeMoLogo" style= {{ display:'block', margin: '0 auto', paddingTop: '50px'}}/>            
             <div style ={{textAlign:'center', marginTop:'50px'}}>
-                <form>
-                    <TextField label="아이디" required /><br/>
-                    <TextField label="비밀번호" required /><br/><br/>
-                    <Button variant="outlined">로그인</Button>&nbsp;
+                <form method="post" onSubmit={loginHandler}>
+                    <TextField label="아이디" value = {UserId} onChange={idInputHandler} required /><br/>
+                    <TextField label="비밀번호" type="password" value = {UserPassword} onChange={pwInputHandler} required /><br/><br/>
+                    <Button variant="outlined" onClick = {loginHandler}>로그인</Button>&nbsp;
                     <Button variant="outlined" href='/register'>회원가입</Button>
                 </form>
             </div>
